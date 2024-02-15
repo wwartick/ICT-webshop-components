7f614e9c6a36d70718213a829efb81082364ed3f
wwartick <willwartick@gmail.com>
2024-02-15 08:52:06 -0800

    css + js for slideshow

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const activeCat = document.querySelectorAll('.category')
+let slideIndex=1;
+
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex +=n);
+}
+
+function showDivs(n) {
+    var i; 
+    var allProducts = document.getElementsByClassName('products')
+    if (n > allProducts.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = allProducts.length} ;
+    for (i = 0; i < allProducts.length; i++) {
+    allProducts[i].style.display = "none";
+  }
+    allProducts[slideIndex-1].style.display = "block";
+
+}
 
 const changeActive = function(e) {
     const target=e.target;
